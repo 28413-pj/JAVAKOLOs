eda6599f226cb12f1f23177734470897deaaf1ad
s28413-pj <117545944+28413-pj@users.noreply.github.com>
2024-01-27 09:53:38 +0100

    Create Klient.java

---
+++ Klient.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+public class Klient {
+    public int ID;
+
+    public Klient(int Id) {
+        this.ID = ID;
+    }
+
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+}
